7b73bf8f8c2bc0248fa98e9fe2ef6b0d91affe84
DANIEL PANIAGUA VARGAS <dpaniagua01@estudiantec.cr>
2025-02-28 02:17:42 -0600

    [Corección] 3 errores flake 8
    Se corrigieron los 3 errores identificables por flake 8 del "archivo_errores.py".

--- Tarea 1/archivo_errores.py
+++ Tarea 1/archivo_errores.py
@@ -1,4 +1,4 @@
-def funcion_con_errores(parametro1, parametro2): 
-    print ("Error de espacio después de paréntesis")  # E211
-    resultado = parametro1+ parametro2  # E255
+def funcion_con_errores(parametro1, parametro2):
+    print("Error de espacio después de paréntesis")
+    resultado = parametro1 + parametro2
     return resultado
